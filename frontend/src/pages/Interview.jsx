8a7f27903ec9f75ce9a4114ecb237abdf8ab9833
paramdahiya <paramdahiya06@gmail.com>
2026-04-01 15:00:40 +1100

    creating interview report form

--- frontend/src/pages/Interview.jsx
+++ frontend/src/pages/Interview.jsx
@@ -1,30 +1,51 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function () {
+    const [jobDescription, setJobDescription] = useState("");
+    const [summary, setSummary] = useState("");
+
   return (
-    <div className='flex flex-col w-full items-center gap-4'>
+    <div className='flex flex-col w-full items-center gap-4 p-2 md:p-0'>
         
         <h1 
             className='inline-block bg-linear-to-r from-[#38BDF8] to-[#1D4ED8] text-transparent bg-clip-text font-playfair text-5xl font-bold leading-none mt-30'>
                 Create Your Custom Interview Plan
         </h1>
         <p 
-            className='text-[#94A3B8] text-base text-center'>
+            className='text-[#94A3B8] text-base'>
                 Let Gemini AI analyse the job requirements and your unique profile to build a winning strategy.
         </p>
 
-        <div className='grid grid-cols-2 border bg-[#1E293B]'>
-            
-            <div className='flex flex-col border border-[#334155]'>
-                <label className='text-[#F8FAFC]' htmlFor="jd">Target Job Description</label>
-                <textarea className='text-[#94A3B8]' name="jd" id="jd">Enter the job description....</textarea>
-            </div>
+        <div className='w-full md:w-7xl bg-[#1E293B]'>
+            <div className='w-full grid grid-cols-2'>
+                
+                <div className='w-full flex flex-col p-2 gap-2'>
+                    <label 
+                        className='text-[#F8FAFC] text-lg'
+                        htmlFor="jd"
+                    >
+                            Job Description
+                    </label>
+                    <textarea 
+                        name="jd" 
+                        id="jd"
+                        className='text-[#94A3B8] h-100 w-full border border-[#334155] pl-2 focus:outline-none focus:ring-1 focus:ring-[rgba(56, 189, 248, 0.5)] transition-all duration-200 focus:shadow-xl focus:scale-[1.01]'
+                        placeholder='Enter the job description...'
+                    >
+                    </textarea>
+                </div>
 
-            <div className='flex flex-col border border-[#334155]'>
-                <label className='text-[#F8FAFC]' htmlFor="jd">Target Job Description</label>
-                <textarea className='text-[#94A3B8]' name="jd" id="jd">Enter the job description....</textarea>
-            </div>
+                <div className='w-full'>
+                    <label 
+                        className='text-[#F8FAFC] text-lg'
+                        htmlFor="jd"
+                    >
+                            Resume
+                    </label>
+                </div>
 
+            </div>
         </div>
     </div>
   )

43d8963e5fe9bfeaa1ea131861bd77c8ecde7e4a
paramdahiya <paramdahiya06@gmail.com>
2026-04-07 12:27:24 +1000

    implemented the sample resume analysis card in UI

---
+++ frontend/src/components/GoodInfoComponent.jsx
@@ -0,0 +1,18 @@
+// component to show the AI generated info - Critical, Warning, Good
+
+import React from 'react'
+
+export default function InfoComponent({issue}) {
+    return (
+        <div className='flex gap-2 sm:gap-4 bg-[#1E293B] border border-[#334155] shadow-lg p-6'>
+            <div className='self-start bg-[#7DD3FC]/50 text-[#7DD3FC] px-1 py-0.5 text-sm'>
+                <h2>GOOD</h2>
+            </div>
+            <div className=''>
+                <p className='text-[#94A3B8]'>
+                    {issue}
+                </p>
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,9 @@ import Header from '../components/Header'
 import '../css/home.css'
 import Button from '../components/Button'
 import ScoreCard from '../components/ScoreCard'
+import CriticalInfoComponent from '../components/CriticalInfoComponent'
+import GoodInfoComponent from '../components/GoodInfoComponent'
+import WarningInfoComponent from '../components/WarningInfoComponent'
 
 const dummyData = {
     score:62,
@@ -15,7 +18,6 @@ const dummyData = {
     criticalIssues: 
     [
         "No quantified achievements - add metrics to bullet points",
-
     ],
     NumWarnings:2,
     warnings:
@@ -34,8 +36,8 @@ export default function Home() {
     return (
         <div className='w-full'>
             <Header/>
-            <div className='p-16'>
-                <div className='grid grid-cols-1 sm:grid-cols-2 gap-12'>
+            <div className='p-12 sm:p-16'>
+                <div className='grid grid-cols-1 sm:grid-cols-[60fr_40fr] gap-12'>
                     
                     <div className='flex flex-col gap-4'>
                         
@@ -78,29 +80,45 @@ export default function Home() {
                     {/* second column */}
                     
                     <div className='border-t border-t-[#0EA5E9] bg-[#1E293B] p-2 text-[#F8FAFC] flex flex-col shadow-xl pl-5'>
+                        
                         <h2 className='text-center inline-block bg-linear-to-r from-[#38BDF8] to-[#1D4ED8] text-transparent bg-clip-text font-playfair font-black text-base mt-3'>RESUME ANALYSIS</h2>
-
-                        <div className='grid grid-cols-2 mt-6'>
+                        
+                        <div className='grid grid-cols-1 gap-2 sm:gap-0 sm:grid-cols-2 mt-6 justify-items-center '>
                             <div className=''>
                                 <div className="outer flex flex-col">
                                     {/* dummy data */}
                                     <div class="inner flex flex-col">
-                                        <span className='text-[#F8FAFC] text-3xl'>{dummyData.score}</span>
+                                        <span className='text-[#7DD3FC] text-3xl'>{dummyData.score}</span>
                                         <p className='text-[#94A3B8]'>Score</p>
                                     </div>
                                 </div>
                             </div>
 
-                            <div className='flex flex-col'>
+                            <div className='flex flex-col items-center sm:items-start'>
                                 <h2 className='text-[#F8FAFC] text-xl'>{dummyData.overallAnalysis}</h2>
-                                <p className='text-[#94A3B8]'>{dummyData.reviewSummary}</p>
+                                <p className='text-center sm:text-left text-[#94A3B8]'>{dummyData.reviewSummary}</p>
                             </div>
                         </div>
 
-                        <div className='flex flex-col sm:flex-row mt-10 border border-[#334155] bg-[#1E293B] shadow-lg p-5 justify-between'>
+                        <div className='flex flex-col sm:flex-row mt-10 border border-[#334155] bg-[#1E293B] shadow-lg p-5 gap-4 sm:justify-between'>
                             <ScoreCard name="IMPACT" score={dummyData.impact}></ScoreCard>
                             <ScoreCard name="CLARITY" score={dummyData.clarity}></ScoreCard>
                             <ScoreCard name="ATS SCORE" score={dummyData.atsScore}></ScoreCard>
+                        </div>
+
+                        {/* info components */}
+                        <div className='flex flex-col mt-6 gap-4'>
+                            {dummyData.criticalIssues.map((issue, i)=>{
+                                return <CriticalInfoComponent issue={issue}/>
+                            })}
+
+                            {dummyData.warnings.map((warning, i)=>{
+                                return <WarningInfoComponent issue={warning}/>
+                            })}
+
+                            {dummyData.good.map((good, i)=>{
+                                return <GoodInfoComponent issue={good}/>
+                            })}
                         </div>
                     
                     </div>
